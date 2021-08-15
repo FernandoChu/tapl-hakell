d24e3404179df4f82d87e75c5ef32e276a4ebb68
fernando <fernandochu97@gmail.com>
2021-08-15 17:35:18 -0500

    generalized running types

--- src/Arith/Core.hs
+++ src/Arith/Core.hs
@@ -20,30 +20,38 @@ import           Arith.Print                    ( Print
                                                 )
 import           Arith.Skel                     ( )
 import           System.Console.Haskeline
-import           TaplTypes
-
-type ParseFun a = [Token] -> Err a
+import           Types
 
 putStrV :: Verbosity -> String -> IO ()
 putStrV Normal  s = pure ()
 putStrV Verbose s = putStrLn s
 
-runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
+runFile
+  :: (GExp a, Print a, Show a, GToken tok)
+  => Verbosity
+  -> ParseFun tok a
+  -> FilePath
+  -> IO ()
 runFile v p f = putStrLn f >> readFile f >>= run v p
 
-run :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
+run
+  :: (GExp a, Print a, Show a, GToken tok)
+  => Verbosity
+  -> ParseFun tok a
+  -> String
+  -> IO ()
 run v p s = case p ts of
   Left err -> do
     putStrLn "\nParse              Failed...\n"
     putStrV v "Tokens:"
-    mapM_ (putStrV v . showPosToken . mkPosToken) ts
+    mapM_ (putStrV v . showPosToken . gMkPosToken) ts
     putStrV v "Tokens:"
     putStrLn err
   Right tree -> do
     showTree v tree
     forM_ (reverse (eval v tree)) print
  where
-  ts = myLexer s
+  ts = gLexer s
   showPosToken ((l, c), t) = concat [show l, ":", show c, "\t", show t]
 
 showTree :: (Show a, Print a) => Verbosity -> a -> IO ()
@@ -61,40 +69,23 @@ isVal ETrue  = True
 isVal EFalse = True
 isVal t      = isNumericVal t
 
-eval1 :: Exp -> Either EvaluationError Exp
-eval1 (ECond ETrue  t2 _ ) = Right t2
-eval1 (ECond EFalse _  t3) = Right t3
-eval1 (ECond t1 t2 t3) =
-  let t1' = eval1 t1 in t1' >>= Right . (\x -> ECond x t2 t3)
-eval1 (ESucc t1   ) = let t1' = eval1 t1 in t1' >>= Right . ESucc
-eval1 (EPred EZero) = Right EZero
-eval1 (EPred (ESucc t)) =
-  if isNumericVal t then Right t else Left (EvaluationError $ show t)
-eval1 (EIsZero EZero ) = Right ETrue
-eval1 (EIsZero ETrue ) = Right EFalse
-eval1 (EIsZero EFalse) = Right EFalse
-eval1 (EIsZero (ESucc t)) =
-  if isNumericVal t then Right EFalse else Left (EvaluationError $ show t)
-eval1 (EIsZero t1) = let t1' = eval1 t1 in t1' >>= Right . EIsZero
-eval1 _            = Left $ EvaluationError "Normal form term"
+instance GExp Exp where
+  eval1 (ECond ETrue  t2 _ ) = Right t2
+  eval1 (ECond EFalse _  t3) = Right t3
+  eval1 (ECond t1 t2 t3) =
+    let t1' = eval1 t1 in t1' >>= Right . (\x -> ECond x t2 t3)
+  eval1 (ESucc t1   ) = let t1' = eval1 t1 in t1' >>= Right . ESucc
+  eval1 (EPred EZero) = Right EZero
+  eval1 (EPred (ESucc t)) =
+    if isNumericVal t then Right t else Left (EvaluationError $ show t)
+  eval1 (EIsZero EZero ) = Right ETrue
+  eval1 (EIsZero ETrue ) = Right EFalse
+  eval1 (EIsZero EFalse) = Right EFalse
+  eval1 (EIsZero (ESucc t)) =
+    if isNumericVal t then Right EFalse else Left (EvaluationError $ show t)
+  eval1 (EIsZero t1) = let t1' = eval1 t1 in t1' >>= Right . EIsZero
+  eval1 _            = Left $ EvaluationError "Normal form term"
 
--- eval :: Verbosity -> Exp -> IO ()
--- eval v t = case eval1 t of
---   Left  err -> print t
---   Right val -> do
---     putStrLn "Here"
---     case eval1 val of
---       Left  err  -> print t
---       Right nval -> do
---         print val
---         eval v val
---     -- print val
---     -- eval v val
-
-eval :: Verbosity -> Exp -> [Exp]
-eval v t = accEval v t [t]
- where
-  accEval v t l = case eval1 t of
-    Left  err -> l
-    Right val -> accEval v val (val : l)
-
+instance GToken Token where
+  gLexer      = myLexer
+  gMkPosToken = mkPosToken
--- src/Types.hs
+++ src/Types.hs
@@ -1,18 +1,22 @@
-module TaplTypes where
+module Types where
 
 data Verbosity = Normal | Verbose
 
 type Err = Either String
--- type ParseFun tok a = [tok] -> Err a
+type ParseFun tok a = [tok] -> Err a
 
 newtype EvaluationError = EvaluationError String
   deriving (Show)
 
--- class GExp a where
---     eval1 :: a -> Either EvaluationError a
---     eval :: Verbosity -> a -> [a]
---     eval v t = accEval v t [t]
---       where
---         accEval v t l = case eval1 t of
---             Left  err -> l
---             Right val -> accEval v val (val : l)
+class GExp a where
+  eval1 :: a -> Either EvaluationError a
+  eval :: Verbosity -> a -> [a]
+  eval v t = accEval v t [t]
+    where
+      accEval v t l = case eval1 t of
+          Left  err -> l
+          Right val -> accEval v val (val : l)
+
+class GToken tok where
+  gLexer :: String -> [tok]
+  gMkPosToken :: tok -> ((Int, Int), String)
