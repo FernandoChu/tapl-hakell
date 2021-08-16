6a4a3f324e8a8f9e8748367e2ae409859577b695
fernando <fernandochu97@gmail.com>
2021-08-15 20:19:35 -0500

    cleaning cmd.hs

--- src/CommandLine/Cmd.hs
+++ src/CommandLine/Cmd.hs
@@ -1,16 +1,43 @@
+-- This program takes huge inspiration from the
+-- Test.hs program generated from the BNFC Converter
+
 module CommandLine.Cmd where
 
-import           Arith.Abs
 import qualified Arith.Core                    as Arith
-import           Arith.Lex
-import           Arith.Par
-import           Arith.Print
-import           Arith.Skel
-import           Control.Monad                  ( forever )
+import qualified Arith.Par                      ( pExp )
+import           Arith.Print                    ( Print
+                                                , printTree
+                                                )
+import           Control.Monad                  ( forM_
+                                                , forever
+                                                )
 import           Control.Monad.Trans.Class      ( lift )
 import           Data.Semigroup                 ( (<>) )
-import           Options.Applicative
-import           System.Console.Haskeline
+import           Options.Applicative            ( (<**>)
+                                                , Parser
+                                                , ParserInfo
+                                                , auto
+                                                , command
+                                                , execParser
+                                                , flag
+                                                , footerDoc
+                                                , fullDesc
+                                                , header
+                                                , help
+                                                , helper
+                                                , info
+                                                , long
+                                                , metavar
+                                                , option
+                                                , progDesc
+                                                , short
+                                                , strArgument
+                                                , subparser
+                                                )
+import           System.Console.Haskeline       ( defaultSettings
+                                                , getInputLine
+                                                , runInputT
+                                                )
 import           System.Environment             ( getArgs )
 import           System.Exit                    ( exitFailure
                                                 , exitSuccess
@@ -23,7 +50,7 @@ data Language = Arith
   deriving (Read)
 
 data Command = Repl
-             | EvalFile String
+             | EvalFile FilePath
              | EvalStdin
   deriving (Read)
 
@@ -69,109 +96,84 @@ mainCmd :: IO ()
 mainCmd = do
   opts <- execParser optsParser
   case optCommand opts of
-    Repl -> forever $ runInputT defaultSettings $ do
-      ml <- getInputLine "% "
-      case ml of
-        Nothing -> pure ()
-        Just l  -> lift $ Arith.run (verbosity opts) pExp l
-    EvalFile a -> greet opts
-    EvalStdin  -> greet opts
+    Repl        -> runRepl (verbosity opts) Arith.Par.pExp
+    EvalFile fs -> runFile (verbosity opts) Arith.Par.pExp fs
+    EvalStdin   -> getLine >>= run (verbosity opts) Arith.Par.pExp
+
+putStrV :: Verbosity -> String -> IO ()
+putStrV v s = case v of
+  Normal  -> pure ()
+  Verbose -> putStrLn s
+
+runFile
+  :: (GExp a, Print a, Show a, GToken tok)
+  => Verbosity
+  -> ParseFun tok a
+  -> FilePath
+  -> IO ()
+runFile v p f = putStrLn f >> readFile f >>= run v p
+
+runRepl
+  :: (GExp a, Print a, Show a, GToken tok)
+  => Verbosity
+  -> ParseFun tok a
+  -> IO ()
+runRepl v p = forever $ runInputT defaultSettings $ do
+  ml <- getInputLine "% "
+  case ml of
+    Nothing -> pure ()
+    Just l  -> lift $ run v p l
+
+run
+  :: (GExp a, Print a, Show a, GToken tok)
+  => Verbosity
+  -> ParseFun tok a
+  -> String
+  -> IO ()
+run v p s = case p ts of
+  Left err -> do
+    putStrLn "\nParse              Failed...\n"
+    putStrV v "Tokens:"
+    mapM_ (putStrV v . showPosToken . gMkPosToken) ts
+    putStrV v "Tokens:"
+    putStrLn err
+  Right tree -> do
+    showTree v tree
+    forM_ (reverse (eval v tree)) print
  where
-  optsParser = info
-    (cmdOptions <**> helper)
-    (  fullDesc
-    <> progDesc "Print a greeting for TARGET"
-    <> header
-         (unlines
-           [ "Command line for the different interpreters in Benjamin Pierce's Types and"
-           , "Programming Languages. Made with Haskell by Ryunaq."
-           ]
-         )
-    <> footerDoc
-         (Just
-           (PrettyDoc.text
-             (unlines
-               [ "Available Languages:"
-               , "  arith                    Implementation of Chapter 3 language"
-               , "  lambda                   Implementation of Chapter 4 language"
-               , ""
-               , "Examples"
-               , "  exe -l=lambda repl   # Starts a repl evaluating untyped lambda expressions"
-               , "  exe -v foo.txt       # Evaluates verbosely the contents of the file foo.txt"
-               , "  echo \"if true then 0 else succ 0\" | exe -"
-               ]
-             )
+  ts = gLexer s
+  showPosToken ((l, c), t) = concat [show l, ":", show c, "\t", show t]
+
+showTree :: (Show a, Print a) => Verbosity -> a -> IO ()
+showTree v tree = do
+  putStrV v $ "\n[Abstract Syntax]\n\n" ++ show tree
+  putStrV v $ "\n[Linearized tree]\n\n" ++ printTree tree
+
+optsParser :: ParserInfo CmdOptions
+optsParser = info
+  (cmdOptions <**> helper)
+  (  fullDesc
+  <> progDesc "Print a greeting for TARGET"
+  <> header
+       (unlines
+         [ "Command line for the different interpreters in Benjamin Pierce's Types and"
+         , "Programming Languages. Made with Haskell by Ryunaq."
+         ]
+       )
+  <> footerDoc
+       (Just
+         (PrettyDoc.text
+           (unlines
+             [ "Available Languages:"
+             , "  arith                    Implementation of Chapter 3 language"
+             , "  lambda                   Implementation of Chapter 4 language"
+             , ""
+             , "Examples"
+             , "  exe -l=lambda repl   # Starts a repl evaluating untyped lambda expressions"
+             , "  exe -v foo.txt       # Evaluates verbosely the contents of the file foo.txt"
+             , "  echo \"if true then 0 else succ 0\" | exe -"
+             ]
            )
          )
-    )
-
-greet :: CmdOptions -> IO ()
-greet (CmdOptions h l n) = putStrLn $ "Hello, " ++ "!"
--- data Language
---   = Arith
---   | Lambda
---   deriving (Show)
--- data Command
---   = Repl
---   | EvalFile
---   | EvalStdin
---   deriving (Show)
--- updateVerbosity :: CmdOptions -> Verbosity -> CmdOptions
--- updateVerbosity cmdopt y = cmdopt {verbosity = y}
--- updateLang :: CmdOptions -> Language -> CmdOptions
--- updateLang cmdopt y = cmdopt {lang = y}
--- updateCommand :: CmdOptions -> Command -> CmdOptions
--- updateCommand cmdopt y = cmdopt {command = y}
--- updateHelp :: CmdOptions -> Bool -> CmdOptions
--- updateHelp cmdopt y = cmdopt {help = y}
--- usage :: IO ()
--- usage = do
---   putStrLn $
---     unlines
---       [ "usage: exe [option]... [command]",
---         "Command line for the different interpreters in Benjamin Pierce's Types and",
---         "Programming Languages. Made with Haskell by Ryunaq.",
---         "",
---         "Available options:",
---         "  -h, --help      Display this help message.",
---         "  -l, --lang      Language implementation to use. See available ones below",
---         "  -v, --verbose   Display parse and evaluation steps",
---         "",
---         "Available implementations:",
---         "  arith           Implementation of Chapter 3 language",
---         "",
---         "Available commands:",
---         "  repl            Makes a repl that constantly evaluates user input",
---         "  eval <file>     Evaluates de contents of a file",
---         "  -               Evaluates de contents from stdin",
---         "",
---         "Examples",
---         "  exe -l=lambda repl   # Starts a repl evaluating untyped lambda expressions",
---         "  exe -v foo.txt       # Evaluates verbosely the contents of the file foo.txt",
---         "  echo \"if true then 0 else succ 0\" | exe -"
---       ]
---   exitFailure
--- defaultOptions = CmdOptions {verbosity = 0, lang = Arith, command = Repl, help = False}
--- parseOptions :: [String] -> Either String CmdOptions
--- parseOptions x = foldr parseOptions (Right defaultOptions) (reverse x)
---   where
---     parseOptions str eitherOptions = do
---       options <- eitherOptions
---       case str of
---         "-v"        -> Right $ updateVerbosity options 1
---         "--verbose" -> Right $ updateVerbosity options 1
---         "-h"        -> Right $ updateVerbosity options 1
---         "--help"    -> Right $ updateVerbosity options 1
---         _           -> Left "Can't parse"
--- cmd :: IO ()
--- cmd = do
---   args <- getArgs
---   case args of
---     "--help":_ -> usage
---     x:xs       ->    getContents >>= run 2 pExp
---   -- args <- getArgs
---   -- case args of
---   --   "--help":_          -> usage
---   --   x:xs             -> getContents >>= run 2 pExp
---   --   "arith" : "-s" : fs -> mapM_ (runFile 0 pExp) fs
---   --   fs                  -> mapM_ (runFile 2 pExp) fs
+       )
+  )
